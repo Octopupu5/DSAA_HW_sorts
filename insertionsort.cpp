b0ad31d156962d007cd52bc4c3677e7e5318ede4
Octopupu5 <pankratovstepan2004@gmail.com>
2023-10-21 18:47:27 +0300

    +generator

--- insertionsort.cpp
+++ insertionsort.cpp
@@ -1,4 +1,5 @@
 #include <bits/stdc++.h>
+#include <chrono>
 using namespace std;
 
 int main (int argc, char **argv) {
@@ -15,12 +16,23 @@ int main (int argc, char **argv) {
     for (auto & el : A)
         cin >> el;
     
+    auto start = chrono::high_resolution_clock::now();
+    ios_base::sync_with_stdio(false);
+
     /* Sorting process */
     for (int i = 0; i < n; ++i) {
         int saved = A[i];
         for (int j = i - 1; j >= 0 && A[j] > saved; --j)
             swap(A[j], A[j + 1]);
     }
+
+    auto end = chrono::high_resolution_clock::now();
+    double time_taken = chrono::duration_cast<chrono::nanoseconds>(end - start).count();
+ 
+    time_taken *= 1e-9;
+ 
+    cout << "Time taken by program is : " << fixed << time_taken << setprecision(9);
+    cout << " sec" << endl;
 
     if (argc == 2) {
         cout << "Sorted array :\n";
